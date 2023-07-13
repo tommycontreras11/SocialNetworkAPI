117787a567b6c6c1e7e8583c339b9ddb16d6b277
Tommy Grullón Contreras <elnegritodepapi@hotmail.com>
2023-07-13 16:25:14 -0400

    Added the user model

---
+++ SocialNetworkAPI/Enums/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetworkAPI.Enums
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+}
---
+++ SocialNetworkAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using SocialNetworkAPI.Enums;
+
+namespace SocialNetworkAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
