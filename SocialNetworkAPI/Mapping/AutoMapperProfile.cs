0286e6d1f766bd55359b362260986c26b99cec21
Tommy Grullón Contreras <elnegritodepapi@hotmail.com>
2023-07-13 17:39:01 -0400

    Configure automapper for user crud

---
+++ SocialNetworkAPI/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace SocialNetworkAPI.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, GetUserDto>();
+            CreateMap<AddUserDto, User>();
+        }
+    }
+}
